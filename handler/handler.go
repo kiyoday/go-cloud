10d4a32e2a1f50f9801ff35935eb27ecdafe5bd6
tangzixu <1260583285@qq.com>
2020-07-25 17:00:16 +0800

    文件元信息更新(name)

--- handler/handler.go
+++ handler/handler.go
@@ -106,3 +106,35 @@ func DownloadHandler(w http.ResponseWriter,r *http.Request){
 	w.Write(data)
 
 }
+
+// 更新元信息接口(重命名)
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	opType := r.Form.Get("op")
+	fileSha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(fileSha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+
+	// TODO: 更新文件表中的元信息记录
+
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main(){
 	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
 	http.HandleFunc("/file/meta",handler.GetFileMetaHandler)
 	http.HandleFunc("/file/download",handler.DownloadHandler)
+	http.HandleFunc("/file/update",handler.FileMetaUpdateHandler)
 	err := http.ListenAndServe(":8090",nil)
 	if err !=nil {
 		fmt.Printf("Failed to start server,err:%s",err.Error())
