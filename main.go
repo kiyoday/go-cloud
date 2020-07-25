a7c92ec45277c0067f03557ae8c602f1f555f7ab
tangzixu <1260583285@qq.com>
2020-07-25 16:19:12 +0800

    文件下载接口

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main(){
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
 	http.HandleFunc("/file/meta",handler.GetFileMetaHandler)
-	//http.HandleFunc("/file/download",handler.DownloadHandler)
+	http.HandleFunc("/file/download",handler.DownloadHandler)
 	err := http.ListenAndServe(":8090",nil)
 	if err !=nil {
 		fmt.Printf("Failed to start server,err:%s",err.Error())
